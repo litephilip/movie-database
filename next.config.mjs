8dd09e6152ac5dcde4c583a8a3debac2c507e941
Philip Gistedt <philip_91_1@hotmail.com>
2024-12-04 11:30:49 +0100

    chore: Update config for static resources from movie posters

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['m.media-amazon.com'],
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback.fs = false;
+        }
+
+        return config;
+    },
+};
 
 export default nextConfig;
